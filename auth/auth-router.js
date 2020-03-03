0871dcf5361d93cef9a859c6641cdb07eb4cc069
Ahmad Guenoun <amguenoun@gmail.com>
2020-03-03 12:48:31 -0500

    added some packages and completed the register endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const db = require('../database/dbConfig');
+const bcrypt = require('bcryptjs');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const credentials = req.body;
+  const passHash = bcrypt.hashSync(credentials.password, 12);
+  credentials.password = passHash;
+
+  db('users')
+    .insert(credentials)
+    .then(userId => {
+      res.status(201).json({ id: userId[0] })
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Uh oh, theres something gone wrong!" })
+    })
 });
 
 router.post('/login', (req, res) => {
