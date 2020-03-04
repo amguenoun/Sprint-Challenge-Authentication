a28af3caa4a77ee535e03561ca99aedc219391fa
Ahmad Guenoun <amguenoun@gmail.com>
2020-03-04 14:32:54 -0500

    added jsonwebtoken package and implemented login endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const db = require('../database/dbConfig');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.post('/register', (req, res) => {
   const credentials = req.body;
@@ -18,7 +19,32 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  const credentials = req.body;
+
+  db('users')
+    .where('username', credentials.username)
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(credentials.password, user.password)) {
+        const payload = {
+          subject: user.id,
+          username: user.username,
+          department: user.department
+        };
+        const options = {
+          expiresIn: '1h'
+        };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, options)
+
+        res.status(200).json({ message: 'welcome', token: token })
+      }
+      else {
+        res.status(400).json({ message: "invalid password" })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Uh oh, theres something gone wrong!" })
+    })
 });
 
 module.exports = router;
